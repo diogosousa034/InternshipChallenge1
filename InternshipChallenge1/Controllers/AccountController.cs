62bd85d912acda5c8df3c77ec9a2aee470b3ebdf
diogosousa034 <96498805+diogosousa034@users.noreply.github.com>
2022-01-12 16:41:32 +0000

    Adição de alguns controles

---
+++ InternshipChallenge1/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using InternshipChallenge1.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static InternshipChallenge1.Models.AccountsDb;
+
+namespace InternshipChallenge1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Account> objList = _db.Accounts;
+            return View(objList);
+        }
+    }
+}
---
+++ InternshipChallenge1/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static InternshipChallenge1.Models.AccountsDb;
+
+namespace InternshipChallenge1.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountsContent> AccountsContents { get; set; }
+        public DbSet<AccountContentComment> AccountContentComments { get; set; }
+    }
+}
