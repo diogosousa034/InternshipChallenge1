7ba86d91c93703e1871c9c996a79149cbc1ea555
diogosousa034 <96498805+diogosousa034@users.noreply.github.com>
2022-01-12 14:30:39 +0000

    Adição de migration

---
+++ InternshipChallenge1/Migrations/20220112142929_addAccountToDatabase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternshipChallenge1.Migrations
+{
+    public partial class addAccountToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    NrFollowers = table.Column<int>(nullable: false),
+                    NrFollowing = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountsContents",
+                columns: table => new
+                {
+                    AccountsContentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<byte[]>(nullable: true),
+                    PublicationData = table.Column<DateTime>(nullable: false),
+                    AccountId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountsContents", x => x.AccountsContentId);
+                    table.ForeignKey(
+                        name: "FK_AccountsContents_Account_AccountsContentId",
+                        column: x => x.AccountsContentId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountContentComments",
+                columns: table => new
+                {
+                    AccountContentCommentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(nullable: true),
+                    AccountsContentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountContentComments", x => x.AccountContentCommentId);
+                    table.ForeignKey(
+                        name: "FK_AccountContentComments_AccountsContents_AccountContentCommentId",
+                        column: x => x.AccountContentCommentId,
+                        principalTable: "AccountsContents",
+                        principalColumn: "AccountsContentId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountContentComments");
+
+            migrationBuilder.DropTable(
+                name: "AccountsContents");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
---
+++ InternshipChallenge1/Models/AccountsDb.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipChallenge1.Models
+{
+    public class AccountsDb
+    {
+        public class AccountContentComment
+        {
+            public int AccountContentCommentId { get; set; }
+            public string Message { get; set; }
+
+            public int AccountsContentId { get; set; }
+            public AccountsContent AccountsContent { get; set; }
+        }
+
+        public class AccountsContent
+        {
+            public int AccountsContentId { get; set; }
+            public byte[] Image { get; set; }
+            public DateTime PublicationData { get; set; }
+
+            public int AccountId { get; set; }
+            public Account Account { get; set; }
+
+            public IList<AccountContentComment> AccountContentComments { get; set; }
+        }
+
+        public class Account
+        {
+            public int Id { get; set; }
+            public string UserName { get; set; }
+            public string FullName { get; set; }
+            public int NrFollowers { get; set; }
+            public int NrFollowing { get; set; }
+
+            public IList<AccountsContent> AccountsContents { get; set; }
+        }
+
+        public class AccountsContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-0U772O3\SQLEXPRESS;Database=AccountsDb;Trusted_Connection=True;");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                //Write Fluent API configurations here
+
+                //Property Configurations
+                modelBuilder.Entity<Account>()
+                        .HasKey(o => o.Id);
+                modelBuilder.Entity<Account>()
+                    .Property(e => e.Id)
+                    .ValueGeneratedOnAdd();
+
+                modelBuilder.Entity<AccountsContent>()
+                        .HasKey(o => o.AccountsContentId);
+                modelBuilder.Entity<AccountsContent>()
+                    .Property(e => e.AccountsContentId)
+                    .ValueGeneratedOnAdd();
+
+                modelBuilder.Entity<AccountContentComment>()
+                        .HasKey(o => o.AccountContentCommentId);
+                modelBuilder.Entity<AccountContentComment>()
+                    .Property(e => e.AccountContentCommentId)
+                    .ValueGeneratedOnAdd();
+
+
+
+                modelBuilder.Entity<AccountsContent>()
+                .HasOne<Account>(s => s.Account)
+                .WithMany(g => g.AccountsContents)
+                .HasForeignKey(s => s.AccountsContentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+                modelBuilder.Entity<AccountContentComment>()
+               .HasOne<AccountsContent>(s => s.AccountsContent)
+               .WithMany(g => g.AccountContentComments)
+               .HasForeignKey(s => s.AccountContentCommentId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            }
+            public DbSet<Account> Account { get; set; }
+            public DbSet<AccountsContent> AccountsContents { get; set; }
+            public DbSet<AccountContentComment> AccountContentComments { get; set; }
+        }
+    }
+}
